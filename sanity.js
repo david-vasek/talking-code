dcc536215ffd1cf52c05d3ce3784ee106bf308d5
David Vasek <david.vasek51@gmail.com>
2022-02-08 08:34:49 -0600

    created sanityJS file to act a config file to pull in data from server

---
+++ sanity.js
@@ -0,0 +1,19 @@
+import {
+  createCurrentUserHook,
+  createClient,
+  createImageUrlBuilder,
+} from 'next-sanity'
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: 'v1',
+
+  useCdn: process.env.NODE_ENV === 'production',
+}
+
+export const sanityClient = createClient(config)
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+
+export const useCurrentUser = createCurrentUserHook(config)
